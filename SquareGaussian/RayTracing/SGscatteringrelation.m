1d42053a7dd868459ddba4ee216226e8d40fa1f8
aaronc8 <aaron.g.chen@gmail.com>
2016-08-29 16:44:17 -0700

    Update SGscatteringrelation.m

--- SquareGaussian/RayTracing/SGscatteringrelation.m
+++ SquareGaussian/RayTracing/SGscatteringrelation.m
@@ -12,37 +12,44 @@ uE = cell((Nedge-2),(Nangle-2));
 % the angle of incidence. 
 
 ds = 1;
-% Only for the West (Left) Edge:
+
 for i = 1:Nedge-2
     for j = 1:Nangle-2
         u0 = [-1; 1-i*dl; cos(j*dphi - pi/2); sin(j*dphi - pi/2)];   % Only for left edge!
         uW{i,j} = squaregaussianrelation(u0,ds);
-    end
-end
-
-% Now for the South (Bottom) Edge:
-for i = 1:Nedge-2
-    for j = 1:Nangle-2
         u0 = [-1 + i*dl; -1; cos(j*dphi); sin(j*dphi)];   % Only for bottom edge!
         uS{i,j} = squaregaussianrelation(u0,ds);
-    end
-end
-
-% Now the East (Right) Edge:
-for i = 1:Nedge-2
-    for j = 1:Nangle-2
         u0 = [1; -1+i*dl; cos(j*dphi + pi/2); sin(j*dphi + pi/2)];   % Only for left edge!
         uE{i,j} = squaregaussianrelation(u0,ds);
-    end
-end
-
-% The North (Top) Edge:
-for i = 1:Nedge-2
-    for j = 1:Nangle-2
         u0 = [1 - i*dl; 1; cos(-j*dphi); sin(-j*dphi)];   % Only for bottom edge!
         uN{i,j} = squaregaussianrelation(u0,ds);
     end
 end
 
-
 end
+
+% Prior stuff just for checking:
+
+% % Now for the South (Bottom) Edge:
+% for i = 1:Nedge-2
+%     for j = 1:Nangle-2
+%         u0 = [-1 + i*dl; -1; cos(j*dphi); sin(j*dphi)];   % Only for bottom edge!
+%         uS{i,j} = squaregaussianrelation(u0,ds);
+%     end
+% end
+% 
+% % Now the East (Right) Edge:
+% for i = 1:Nedge-2
+%     for j = 1:Nangle-2
+%         u0 = [1; -1+i*dl; cos(j*dphi + pi/2); sin(j*dphi + pi/2)];   % Only for left edge!
+%         uE{i,j} = squaregaussianrelation(u0,ds);
+%     end
+% end
+% 
+% % The North (Top) Edge:
+% for i = 1:Nedge-2
+%     for j = 1:Nangle-2
+%         u0 = [1 - i*dl; 1; cos(-j*dphi); sin(-j*dphi)];   % Only for bottom edge!
+%         uN{i,j} = squaregaussianrelation(u0,ds);
+%     end
+% end
