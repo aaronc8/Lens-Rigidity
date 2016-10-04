46d1a1a19197bec3f13405514ae032ffe914b57e
aaronc8 <aaron.g.chen@gmail.com>
2016-10-04 13:46:22 -0700

    Update circlegaussian_scripts.jl

--- SquareGaussian/RayTracing/circlegaussian_scripts.jl
+++ SquareGaussian/RayTracing/circlegaussian_scripts.jl
@@ -3,7 +3,7 @@ using PyPlot
 using Polynomials
 include("RayTracing.jl")
 
-Nrotate = 2^4;
+Nrotate = 2^5;
 Nangle = 2^5;
 dphi = pi/Nangle;
 dtheta = 2*pi/Nrotate;
@@ -17,13 +17,15 @@ for i = 1:Nrotate-1
     for j = 1:Nangle-1
         # ds = j*dphi*(Nangle-j)/Nangle;  # May not want to, so that we can see the whole evolution.
         u0 = [cos(i*dtheta), sin(i*dtheta), cos(i*dtheta + pi/2 + j*dphi), sin(i*dtheta + pi/2 + j*dphi)];
-        ~,u = ODE.ode45(gaussianmetric, u0, [0.0,ds]);
+        ~,u = ode45(gaussianmetric, u0, [0.0,ds]);
         k = find( x -> x[1]^2 + x[2]^2 > 1, u[2:end]);
         if !isempty(k)
             u = u[1:k[1]+1];
         end
+
         # ~,u = ODE.ode45(gaussianmetric, u0, [0.0,ds], stopevent = (s,u) -> ( u[1]^2 + u[2]^2 > 1 ) );
         # Noting how some rays don't make it out yet
+
         u1 = map(z->z[1],u);
         u2 = map(z->z[2],u);
         plot(u1,u2);
@@ -33,10 +35,10 @@ for i = 1:Nrotate-1
           ax[:set_ylim]([-2,2]);
           ax[:set_xlabel]("x-position");
           ax[:set_ylabel]("y-position");
-        theta = 0:0.1:2*pi+0.1;
-        x = cos(theta);
-        y = sin(theta);
-        plot(x,y);
+          theta = 0:0.1:2*pi+0.1;
+          x = cos(theta);
+          y = sin(theta);
+          plot(x,y);
         end
     end
 end
@@ -64,6 +66,7 @@ print(uExit[end,:])
 
 ### Part 3: Entire scattering relation
 # Use the following:
+tic()
 uExitTot = CGscatteringrelation(Nrotate,Nangle);
 
 # Check with the prior one (need to pick NSEW)
@@ -73,3 +76,6 @@ xexit = map(x -> x[1], uExit);
 yexit = map(x -> x[2], uExit);
 display("Check that they lie on the boundary: x^2 + y^2 = 1?")
 display(xexit.^2 + yexit.^2)
+display("Check max across all entries:")
+display(norm(xexit[:].^2+yexit[:].^2,Inf))
+toc()
