6ab572014628753a4b8b6e3496c2415560ca2957
aaronc8 <aaron.g.chen@gmail.com>
2016-11-06 15:50:37 -0800

    Update square_gaussian_solvingscripts.jl

--- RayTracing/square_gaussian_solvingscripts.jl
+++ RayTracing/square_gaussian_solvingscripts.jl
@@ -12,6 +12,12 @@ dphi = pi/Nangle;   # This needs to be taken better care of
 dl = 2/Nedge;
 ds = 2;
 
+###############################################################################
+## Testing Development of the Integration functions for K_i^n
+###############################################################################
+## Generating the metric based on grid points. For now, just discretize the exact one.
+## Then, all the things should look / behave close to the original things we've done.
+
 x = linspace(-1,1,Nedge);
 y = x';
 c2xy = exp(x.^2 .+ y.^2);
@@ -28,10 +34,11 @@ display(norm(c2xy-metric(x,y),Inf))
 # Dx = 1-1/Nx; Dy = 1-1/Ny; hx = 1/Nx; hy = 1/Ny;
 # metric(x,y) = gpre( Dx*(x+hx+Dx/2)/2, Dx*(y+hy+Dx/2)/2 );
 
+###############################################################################
 # Construct the Hamiltonian system using the interpolated metric:
 # How to get Julia to output a function?? If dH is done manually, works fine...
-dHtheta = makeHamiltonianTheta(metric,dmetric);  # Or:
-dH = makeHamiltonian(metric,dmetric);
+dHtheta = makeHamiltonian(metric,dmetric,true);  # Or:
+dH = makeHamiltonian(metric,dmetric,false);
 ## And checked its ray evolution is okay now
 
 ## Warning: May have to use the same form the whole way through! That way,
@@ -40,12 +47,16 @@ dH = makeHamiltonian(metric,dmetric);
 
 ## Check working with scattering relation:
 tic();
-uWt, uSt, uEt, uNt = SGscatteringrelation(1,dHtheta,Nedge,Nangle,ds);
+uWt, uSt, uEt, uNt = SGscatteringrelation(true,dHtheta,Nedge,Nangle,ds);
 toc();  # Holy crap I suck this is so slow compared to the original ... due to eval-ing interpolations??
 
 tic();
-uW, uS, uE, uN = SGscatteringrelation(0,dH,Nedge,Nangle,ds);
+uW, uS, uE, uN = SGscatteringrelation(false,dH,Nedge,Nangle,ds);
 toc();  # and even slower .....
+
+tic();
+uWexact, uSexact, uEexact, uNexact = SGscatteringrelation(true,gaussianmetrictheta,Nedge,Nangle,ds);
+toc()
 
 tic();
 xtexit = map(x -> x[1], uWt);
@@ -53,18 +64,45 @@ ytexit = map(x -> x[2], uWt);
 display("Check every ray exited, max(xexit,yexit) in absolute value:")
 display(max(abs(xtexit),abs(ytexit)))
 display("Check the max across all entries:")
-display(norm(max(abs(xtexit),abs(ytexit))[:] - 1,Inf))
+display(norm(max(abs(xtexit),abs(ytexit))[:] , Inf))
 
 xexit = map(x -> x[1], uW);
 yexit = map(x -> x[2], uW);
 display("Check every ray exited, max(xexit,yexit) in absolute value:")
 display(max(abs(xexit),abs(yexit)))
 display("Check the max across all entries:")
-display(norm(max(abs(xexit),abs(yexit))[:] - 1,Inf))
+display(norm(max(abs(xexit),abs(yexit))[:] , Inf))
 
 display("Check the max across all entries of differences:")
 display(norm(max(abs(xexit - xtexit),abs(yexit-ytexit))[:], Inf))
 toc();
 ## The diference is more noticable because we are using a linear approx to g?
 
-## Next part: compute the path interpolants: e.g.
+xexact = map(x -> x[1], uWexact);
+yexact = map(x -> x[2], uWexact);
+display("Check the max across all entries with exact: (Off due to approx?)")
+display(norm(max(abs(xexact - xtexit),abs(yexact-ytexit))[:], Inf))
+display(norm(max(abs(xexit - xexact),abs(yexit-yexact))[:], Inf))
+
+###############################################################################
+## Next part: compute the path interpolants: e.g. X_g(t,X0).
+# One example:
+Xg,sout = generatePath(dH, [-1,0,cos(pi/4),sin(pi/4)], ds);
+Xgtheta,souttheta = generatePath(dHtheta, [-1,0,pi/4], ds);
+Xexact,sexact = generatePath(gaussianmetric, [-1,0,cos(pi/4),sin(pi/4)], ds);
+Xtheta,stheta = generatePath(gaussianmetrictheta, [-1, 0, pi/4], ds);
+display(" Check the path deviance, Without and With Theta perspective:" )
+t1range = 0:0.01:sexact; t2range = 0:0.01:stheta;
+display(norm(Xg(t1range) - Xexact(t1range), Inf))
+display(norm(Xgtheta(t2range) - Xtheta(t2range), Inf))
+## Also plotting the path was pretty clear :)
+## This lets us use it for the integral for K_n^i now.
+
+###############################################################################
+## Now, we have to compue the Hessian of H (called M), to be used for the Jacobian.
+# But, how to check correctness...? Check with original? ._.
+M = HamiltonianHessForcing(metric,dmetric,false);
+Mtheta = HamiltonianHessForcing(metric,dmetric,true);
+J0 = eye(4,4); J0theta = eye(3,3);
+Jacobian = geodesicJacobian(M,Xg,J0,sout);
+JacobianTheta = geodesicJacobian(Mtheta,Xgtheta,J0theta,souttheta);
