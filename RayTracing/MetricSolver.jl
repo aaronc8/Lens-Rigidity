536f8ccf48d6b595e0989b06187d9d6628d5358a
aaronc8 <aaron.g.chen@gmail.com>
2016-11-22 22:57:47 -0800

    Update MetricSolver.jl

--- RayTracing/MetricSolver.jl
+++ RayTracing/MetricSolver.jl
@@ -1,22 +1,79 @@
 ## Functions used to solve for the Riemannian Metric.
 
-function generateMetric(knots,c2xy)
+function GradHess(cxy,p0,pf)
+## Solve for the derivatives of just c with fft. We can square c or do whatever
+## to it from there.
+
+  fftcxy = fft(cxy[1:end-1, 1:end-1]);
+  fftcxy = fftshift(fftcxy);
+  hy,hx = size(fftcxy);   ## We will only return the middle two quarters.
+  fftgradcxy = zeros(Complex{Float64},hy,hx,2);
+
+# x,y both in [p0,pf]
+  W = 0.5*hx; W = convert(Int64,W);
+  for k = -W:1:W-1  # yrange
+      for m = -W:1:W-1 # xrange
+          fftgradcxy[k+W+1,m+W+1,1] = fftcxy[k+W+1,m+W+1]*2im*pi*m/(pf-p0);
+          fftgradcxy[k+W+1,m+W+1,2] = fftcxy[k+W+1,m+W+1]*2im*pi*k/(pf-p0);
+      end
+  end
+  fftgradcxy[:,:,1] = ifft(ifftshift(fftgradcxy[:,:,1]));
+  fftgradcxy[:,:,2] = ifft(ifftshift(fftgradcxy[:,:,2]));
+
+  ffthesscxy = zeros(Complex{Float64},hy,hx,2,2);
+  for k = -W:1:W-1  # yrange
+      for m = -W:1:W-1 # xrange
+          ffthesscxy[k+W+1,m+W+1,1,1] = fftgradcxy[k+W+1,m+W+1,1]*2im*pi*m/(pf-p0);
+          ffthesscxy[k+W+1,m+W+1,1,2] = fftgradcxy[k+W+1,m+W+1,1]*2im*pi*k/(pf-p0);
+          ffthesscxy[k+W+1,m+W+1,2,1] = fftgradcxy[k+W+1,m+W+1,2]*2im*pi*m/(pf-p0);
+          ffthesscxy[k+W+1,m+W+1,2,2] = fftgradcxy[k+W+1,m+W+1,2]*2im*pi*k/(pf-p0);
+      end
+  end
+  ffthesscxy[:,:,1,1] = ifft(ifftshift(ffthesscxy[:,:,1,1]));
+  ffthesscxy[:,:,1,2] = ifft(ifftshift(ffthesscxy[:,:,1,2]));
+  ffthesscxy[:,:,2,1] = ifft(ifftshift(ffthesscxy[:,:,2,1]));
+  ffthesscxy[:,:,2,2] = ifft(ifftshift(ffthesscxy[:,:,2,2]));
+
+  r0 = convert(Int64, hx/4 + 1); rf = 3*r0 + -2;
+
+  return real(fftgradcxy[r0:rf, r0:rf, : ]), real(ffthesscxy[r0:rf, r0:rf, :, :]);
+  # return fftgradcxy,ffthesscxy
+end
+
+function generateMetric(cxy,dcxy,d2cxy)
 # Takes the computed values of cxy at (x,y) ∈ knots and interpolates.
 
-itp = interpolate(knots, c2xy, Gridded(Linear()));
-metric(x,y) = itp[x,y];
-gradmetric(x,y) = gradient(itp,x,y);  # More efficient to use "gradient!" ?
+# c = interpolate(knots, cxy, Gridded(Linear()));
+# metric(x,y) = itp[x,y];
+# gradmetric(x,y) = gradient(itp,x,y);  # More efficient to use "gradient!" ?
+# hessmetric(x,y) = zeros(2,2);
 
-## BSplines Approach: (Needs rescaling.....)
-## Requires the gridpoints be ODD? So that there's even number on right and left of 0.
-# itp = interpolate(cxy,BSpline(Cubic(Natural())), OnGrid())
-# Nx,Ny = size(cxy);
-# # Renormalize to a unit interval in both dimensions, too:
-# metric(x,y) = itp[0.5*x*(Nx-1) + 0.5*(Nx+1) , 0.5*y*(Ny-1) + 0.5*(Ny+1)];
-# gradmetric(x,y) = gradient(itp, 0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1));
+## BSplines Approach: (Needs a lot of rescaling.....)
+## Requires the gridpoints be Odd? So that there's even number on right and left of 0.
+c = interpolate(cxy,BSpline(Quadratic(Natural())), OnGrid())
+Nx,Ny = size(cxy);
+# Renormalize to a unit interval in both dimensions, too:
+cspd(x,y) = c[0.5*x*(Nx-1) + 0.5*(Nx+1) , 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+
+dcdx = interpolate(dcxy[:,:,1],BSpline(Quadratic(Natural())),OnGrid());
+gradx(x,y) = dcdx[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)]
+dcdy = interpolate(dcxy[:,:,2],BSpline(Quadratic(Natural())),OnGrid());
+grady(x,y) = dcdy[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+gradcspd(x,y) = [gradx(x,y),grady(x,y) ];
+
+d2cdxx = interpolate(d2cxy[:,:,1,1],BSpline(Quadratic(Natural())),OnGrid());
+hessxx(x,y) = d2cdxx[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+d2cdxy = interpolate(d2cxy[:,:,1,2],BSpline(Quadratic(Natural())),OnGrid());
+hessxy(x,y) = d2cdxy[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+d2cdyx = interpolate(d2cxy[:,:,2,1],BSpline(Quadratic(Natural())),OnGrid());
+hessyx(x,y) = d2cdyx[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+d2cdyy = interpolate(d2cxy[:,:,2,2],BSpline(Quadratic(Natural())),OnGrid());
+hessyy(x,y) = d2cdyy[0.5*x*(Nx-1) + 0.5*(Nx+1), 0.5*y*(Ny-1) + 0.5*(Ny+1)];
+
+hesscspd(x,y) = [hessxx(x,y) hessxy(x,y) ; hessyx(x,y) hessyy(x,y)];
 
 # Check the domain, but just return:
-return metric,gradmetric;
+return cspd,gradcspd,hesscspd;
 
 end
 
@@ -96,7 +153,10 @@ kx = find(x -> ( abs(x[1]) >= 1 ), u[2:end]);
 ky = find(x -> ( abs(x[2]) >= 1 ), u[2:end]);
 kshift = 0;
 
-while isempty(kx) && isempty(ky)
+scaling = max(ds,1/ds);
+maxiter = 1;
+
+while isempty(kx) && isempty(ky) && maxiter < 1000*scaling ## In case of trapped rays
     u0 = u[end];
     sadd,uadd = ode45(metric,u0,[0.0,ds]);
     kx = find(x -> ( abs(x[1]) >= 1 ), uadd[2:end]);
@@ -106,6 +166,8 @@ while isempty(kx) && isempty(ky)
     kshift = length(u) - shift;
     s = append!(s,sadd);
     u = append!(u,uadd);
+
+    maxiter = maxiter + 1;
 end
 
 if isempty(kx)
@@ -139,9 +201,20 @@ if length(u0) == 4
   Xg(s) = [xitp[s], yitp[s], vxitp[s], vyitp[s]];
 end
 
-## Construct interpolant for exit time:
+## Construct interpolant for exit time:    ## Maybe we don't need to? It should be
+## given from the original ODE evolution...
+
 u1 = u[k-1]; u2 = u[k];
 s1 = s[k-1][1]; s2 = s[k][1]; spread = s2-s1;
+
+if spread <= 1e-4
+  if length(u1)  == 3
+    return Xgtheta,sout;
+  end
+  if length(u1) == 4
+    return Xg,sout;
+  end
+end
 
 # u1 = u[end-1]; u2 = uf;
 # s1 = s[end-1][1]; s2 = s[end][1]; sf = s2-s1;
@@ -190,30 +263,33 @@ end
 
 ###############################
 
-function HamiltonianHess(g::Function,dg::Function, theta)
+function HamiltonianHess(cxy::Function, Dc::Function, D2c::Function)
 # Form the Hessian (it is almost but not quite the Hessian) of H to
 # Use to compute/solve for the Jacobian of the geodesic. (This is M)
+# Didn't see a way to do this in theta perspective....
 
 ## BE Careful about the ordering!! Perhaps, go the collumnwise convention?
+  function JacobianRHS(u)
 
-if theta == false
-  function M(u)
     M = zeros(4,4);
     ## Build so that if we did reshape(M,4,4) it's the Hessian.
+    spd = cxy(u[1],u[2]);
+    grad = Dc(u[1],u[2]);
+    hess = D2c(u[1],u[2]);
 
     # First 4 are H_x,v
-    grad = dg(u[1],u[2]);
-    M[1] = u[3].*grad[1];
-    M[2] = u[4].*grad[1];
-    M[5] = u[3].*grad[2];
-    M[6] = u[4].*grad[2];
+
+    M[1] = 2*u[3].*grad[1].*spd;
+    M[2] = 2*u[4].*grad[1].*spd;
+    M[5] = 2*u[3].*grad[2].*spd;
+    M[6] = 2*u[4].*grad[2].*spd;
     # Next 4 are -H_x,x
-    M[3] = 0;
-    M[4] = -0.5.*(grad[1]+grad[2]).*(u[3].^2 + u[4].^2);
-    M[7] = M[4];
-    M[8] = 0;
+    M[3] = -(grad[1].^2 + spd.*hess[1,1]).*(u[3].^2 + u[4]).^2;
+    M[4] = -(grad[1].*grad[2] + spd.*hess[2,1]).*(u[3].^2 + u[4]).^2;
+    M[7] = -(grad[1].*grad[2] + spd.*hess[1,2]).*(u[3].^2 + u[4]).^2;# M[4];  # Should equal...
+    M[8] = -(grad[2].^2 + spd.*hess[2,2]).*(u[3].^2 + u[4]).^2;
     # Next 4 are H_v,v
-    M[9] = g(u[1],u[2]);
+    M[9] = spd.^2;
     M[10] = 0;
     M[13] = 0;
     M[14] = M[9];
@@ -223,51 +299,86 @@ if theta == false
     M[15] = -M[2];
     M[16] = -M[6];
     return M;
-  end
-  #
-  # function F(s,J)
-  #   # Assume that J is still a matrix!
-  #   return M(s,J)*J;
-  # end
+ end
 
-  return M;
+  return JacobianRHS;
+
 end
 
-if theta == true
-  function Mtheta(u)
-    Mtheta = zeros(3,3);
-    c = g(u[1],u[2])^0.5;
-    grad = dg(u[1],u[2]);
-    xvel = cos(u[3]);
-    yvel = sin(u[3]);
-    # H_theta,x:
-    Mtheta[1] = 0.5.*xvel.*grad[1]./c;
-    Mtheta[2] = 0.5.*yvel.*grad[1]./c;
-    Mtheta[4] = 0.5.*xvel.*grad[2]./c;
-    Mtheta[5] = 0.5.*yvel.*grad[2]./c;
-    # H_x,x
-    g21 = grad[1]+grad[2];
-    Mtheta[3] = -(xvel.*g21 + yvel./(2*c.^2).*grad[1].^2 - xvel.*grad[1].*grad[2]./(2*c.^2))./(2*c);
-    Mtheta[6] = -(yvel.*g21 - xvel./(2*c.^2).*grad[2].^2 + yvel.*grad[1].*grad[2]./(2*c.^2))./(2*c);
-    # H_theta,theta
-    Mtheta[7] = -yvel.*c;
-    Mtheta[8] = xvel.*c;
-    # H_x,theta
-    Mtheta[9] = -(xvel*grad[1] + yvel*grad[2])./(2*c);
-    return Mtheta;
-  end
-
-  # function Ftheta(s,J)
-  #   # Assume that J is still a matrix!
-  #   return Mtheta(s,j)*J;
-  # end
-  #
-  # return M;
-end
+# function HamiltonianHess(g,dg,theta)
+# if theta == false
+#   function M(u)
+#     M = zeros(4,4);
+#     ## Build so that if we did reshape(M,4,4) it's the Hessian.
 #
-# return M;
+#     # First 4 are H_x,v
+#     grad = dg(u[1],u[2]);
 #
-end
+#     M[1] = u[3].*grad[1];
+#     M[2] = u[4].*grad[1];
+#     M[5] = u[3].*grad[2];
+#     M[6] = u[4].*grad[2];
+#     # Next 4 are -H_x,x
+#     M[3] = 0;
+#     M[4] = -0.5.*(grad[1]+grad[2]).*(u[3].^2 + u[4].^2);
+#     M[7] = M[4];
+#     M[8] = 0;
+#     # Next 4 are H_v,v
+#     M[9] = g(u[1],u[2]);
+#     M[10] = 0;
+#     M[13] = 0;
+#     M[14] = M[9];
+#     # Last 4 are -H_v,x = -H_x,v transpose!
+#     M[11] = -M[1];
+#     M[12] = -M[5];
+#     M[15] = -M[2];
+#     M[16] = -M[6];
+#     return M;
+#   end
+#   #
+#   # function F(s,J)
+#   #   # Assume that J is still a matrix!
+#   #   return M(s,J)*J;
+#   # end
+#
+#   return M;
+# end
+#
+# if theta == true
+#   function Mtheta(u)
+#     Mtheta = zeros(3,3);
+#     c = g(u[1],u[2])^0.5;
+#     grad = dg(u[1],u[2]);
+#     xvel = cos(u[3]);
+#     yvel = sin(u[3]);
+#     # H_theta,x:
+#     Mtheta[1] = 0.5.*xvel.*grad[1]./c;
+#     Mtheta[2] = 0.5.*yvel.*grad[1]./c;
+#     Mtheta[4] = 0.5.*xvel.*grad[2]./c;
+#     Mtheta[5] = 0.5.*yvel.*grad[2]./c;
+#     # H_x,x
+#     g21 = grad[1]+grad[2];
+#     Mtheta[3] = -(xvel.*g21 + yvel./(2*c.^2).*grad[1].^2 - xvel.*grad[1].*grad[2]./(2*c.^2))./(2*c);
+#     Mtheta[6] = -(yvel.*g21 - xvel./(2*c.^2).*grad[2].^2 + yvel.*grad[1].*grad[2]./(2*c.^2))./(2*c);
+#     # H_theta,theta
+#     Mtheta[7] = -yvel.*c;
+#     Mtheta[8] = xvel.*c;
+#     # H_x,theta
+#     Mtheta[9] = -(xvel*grad[1] + yvel*grad[2])./(2*c);
+#     return Mtheta;
+#   end
+#
+#   # function Ftheta(s,J)
+#   #   # Assume that J is still a matrix!
+#   #   return Mtheta(s,j)*J;
+#   # end
+#   #
+#   # return M;
+# end
+# #
+# # return M;
+# #
+# end
 
 
 ################################
@@ -302,9 +413,9 @@ end
 
 #################################
 
-function linearMismatch()
-# Compute the integral of the mismatch of geodesic evolution.
-
-end
+# function linearMismatch()
+# # Compute the integral of the mismatch of geodesic evolution, the K_n^i
+#
+# end
 
 ##################################
