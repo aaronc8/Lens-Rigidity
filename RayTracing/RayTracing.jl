4c7f06cedfd5b5fe4c0e18d8c8d0eb319630c120
aaronc8 <aaron.g.chen@gmail.com>
2016-12-14 13:31:36 -0800

    Update RayTracing.jl
    Include exit time when generating the rays.

--- RayTracing/RayTracing.jl
+++ RayTracing/RayTracing.jl
@@ -1,5 +1,5 @@
-## Only for Square, Circle cases. See C1RayTracing for when 
-## the boundary can be given by a polar curve. 
+## These are functions for Ray Tracing with just two basic shapes, Square and Circle.
+## Mainly, just the square, because Circles are smooth and we have SmoothRayTracing.jl
 
 function gaussianmetric(s,u)
 # u(1) = x coordinate, u(2) = y coordinate,
@@ -31,7 +31,8 @@ function gaussianmetrictheta(s,u)
       dH[2] = sin(u[3]).*exp(u[1].^2/2 + u[2].^2/2);
       # The momenta:
       dH[3] = (u[1].*sin(u[3]) - u[2].*cos(u[3])).*exp(u[1].^2/2 + u[2].^2/2);
-      return dH
+
+      return dH;
 end
 
 
@@ -50,7 +51,7 @@ end
 #####################################################################
 
 
-function squaregaussianrelation(u0,ds)
+function squaregaussianrelation(metric::Function,u0,ds)
   # This will take an initial condition and evolve the ODE for the scattering
   # relation. It will also make sure to get when it exits, and return the
   # exit position and velocity.
@@ -58,33 +59,54 @@ function squaregaussianrelation(u0,ds)
 
   # options = odeset('Events',sgEventsFcn);   #### This is where we have problems??
 
-  s,u = ode45(gaussianmetric, u0, [0.0,ds]); #options);  # with official ODE.jl
+  s,u = ode45(metric, u0, [0.0,ds]); #options);  # with official ODE.jl
   # ~,u = ODE.ode45(gaussianmetric, u0, [0.0,ds], stopevent = (s,u) -> (abs(u[1]) > 1 || abs(u[2]) > 1));
   # [~,u] = ode45(@gaussianmetric, [0,1], u0);  % for example
   # it should be kept adapative for the interval of length
-  kx = find(x -> ( abs(x[1]) >= 1 ), u[2:end]);
-  ky = find(x -> ( abs(x[2]) >= 1 ), u[2:end]);
+  heaviside(x::AbstractFloat) = ifelse(x < 0, zero(x), ifelse(x > 0, one(x), oftype(x,0.5)));
+  square(x,y) =  - heaviside(x.+1.0).*heaviside(-x.+1.0).*heaviside(-y.+1.0).*heaviside(y.+1.0);
 
-  while isempty(kx) && isempty(ky)
-      u0 = u[end];
-      s,u = ode45(gaussianmetric,u0,[0.0,ds]);
-      kx = find(x -> ( abs(x[1]) >= 1 ), u[2:end]);
-      ky = find(x -> ( abs(x[2]) >= 1 ), u[2:end]);
+  k = find(x -> ( square(x[1],x[2]) >= -eps() ), u[2:end]);
+  kshift = 0;
+
+  scaling = max(ds,1/ds);
+  maxiter = 1;
+
+  while isempty(k) && maxiter < 1000*scaling ## In case of trapped rays
+    u0 = u[end];
+    sadd,uadd = ode45(metric,u0,[0.0,ds]);
+    k = find(x -> ( square(x[1],x[2]) >= -eps() ), uadd[2:end]);
+    sadd = sadd + s[end];
+
+    kshift = length(u);
+    s = append!(s,sadd);
+    u = append!(u,uadd);
+    # k = find(x -> ( square(x[1],x[2]) >= -eps() ), u[2:end]);
+    maxiter = maxiter + 1;
+end
+
+if isempty(k)
+  k = length(u);
+else
+  k = k[1] + 1 + kshift;
+end
+
+  if maxiter >= 1000*scaling   # Suspect trapped rays.
+    for k = 1:length(uf)
+      uf[k]=Inf;
+    end
+    return append!(uf,Inf);
   end
 
-  if isempty(kx)
-     k = ky[1]+1;
-  end
-  if isempty(ky)
-     k = kx[1]+1;
-  end
-  if !isempty(kx) && !isempty(ky)
-     k = min(kx[1],ky[1]) + 1;
-  end
 
   # Construct interpolant:
   u1 = u[k-1]; u2 = u[k];
-  s1 = s[k-1][1]; s2 = s[k][1]; spread = s2-s1;
+  s1 = s[k-1][1]; s2 = s[k][1]; spread = s2-s1; sout = s1+0.5*spread;
+
+  if abs(u2[1]-u1[1]) <= 1e-4 && abs(u2[2]-u1[2]) <= 1e-4    # Then we are basically fine....take an average.
+    uf = (u2 + u1)./2;
+    return append!(uf,sout);
+  end
 
   # u1 = u[end-1]; u2 = uf;
   # s1 = s[end-1][1]; s2 = s[end][1]; sf = s2-s1;
@@ -93,27 +115,23 @@ function squaregaussianrelation(u0,ds)
   dt = spread/4.0;
   tspan = 0:dt:spread;
   # sp,up = ODE.ode45(gaussianmetric,u1,tspan);
-  s,u = ode45(gaussianmetric,u1,tspan);    # is still being adaptive???
+  s,u = ode45(metric,u1,tspan; points=:specified);    # is still being adaptive???
   up = u[1:5]; sp = s[1:5] + s1;
   up[end] = u2; sp[end] = s2;
   # Interpolate positions:
   xp = map( v -> v[1], up);
-  xI = polyfit(sp,xp);
+  xI = polyfit(sp, xp);
   yp = map( v-> v[2], up);
   yI = polyfit(sp, yp);
 
-  # Interpolate velocity:
-  vxp = map( v -> v[3], up); vyp = map( v-> v[4], up);
-  vxI = polyfit(sp,vxp); vyI = polyfit(sp, vyp);
-
-uf = u2; # Need to use for corner cases, if both |x|,|y| > 1.
+  uf = u2; # Need to use for corner cases, if both |x|,|y| > 1.
 
   # Because it's not root finding the exit point:
   if abs(uf[1]) >= 1  # x-exit
     dom = xI - sign(u2[1]);
     dxI = polyder(xI);
     sout = newtonbisection(dom, dxI, s1, s2, 1e-5);
-    uf = [xI(sout), yI(sout), vxI(sout), vyI(sout)];
+    uf[1] = xI(sout); uf[2] = yI(sout);
   end
 
 
@@ -121,92 +139,26 @@ uf = u2; # Need to use for corner cases, if both |x|,|y| > 1.
     dom = yI - sign(uf[2]);
     dyI = polyder(yI);
     sout = newtonbisection(dom, dyI, s1, s2, 1e-5);
-    uf = [xI(sout), yI(sout), vxI(sout), vyI(sout)];
+    uf[1] = xI(sout); uf[2] = yI(sout);
   end
 
+  # Interpolate velocity:
+  for k = 1:length(uf)-2
+    vp = map( t -> t[k+2], up);
+    v = polyfit(sp,vp);
+    uf[k+2] = v(sout);
+  end
 
-  return uf;
+  return append!(uf,sout);
 
 end
 
-
-### Prior without events:
-
-# kx = find(x -> ( abs(x[1]) >= 1 ), u[2:end]);
-# ky = find(x -> ( abs(x[2]) >= 1 ), u[2:end]);
-#
-# if isempty(kx) && isempty(ky)
-#     u0 = u[end];
-#     uf = squaregaussianrelation(u0,ds);
-#     return uf;
-# end
-#
-# if isempty(kx)
-#    k = ky[1]+1;
-# end
-# if isempty(ky)
-#    k = kx[1]+1;
-# end
-# if !isempty(kx) && !isempty(ky)
-#    k = min(kx[1],ky[1]) + 1;
-# end
-#
-# uf = u[k];
-# dy = u[k][2]-u[k-1][2];
-# dx = u[k][1]-u[k-1][1];
-# m = dy/dx;
-# # Can we just use slope m (or, dy and dx) for the directions/velocities?
-#
-# if abs(uf[1]) > 1
-#
-#     uf[1] = uf[1]/abs(uf[1]);   # So it preserves the sign.
-#
-#     # Interpolated y component
-#     uf[2] = u[k-1][2] + m*(uf[1]-u[k-1][1]);
-#
-#     # Interpolated velocities
-#     uf[3] = dx;
-#     uf[4] = dy;
-#
-#     # To check difference between two interpolation approaches:
-#     # Use r to interpolate exit velocities with weighted averaging:
-#     # r = (uf(1)-u(k-1,1))/(u(k,1)-u(k-1,1));
-#     # uf(3) = uf(3) - (u(k-1,3) + r*(u(k,3) - u(k-1,3)));
-#     # uf(4) = uf(4) - (u(k-1,4) + r*(u(k,4) - u(k-1,4)));
-#
-# end
-#
-# if abs(uf[2]) > 1
-#
-#     uf[2] = uf[2]/abs(uf[2]);   # So it preserves the sign.
-#
-#     # Interpolated x component
-#     uf[1] = u[k-1][1] + (uf[2]-u[k-1][2])/m;
-#
-#     # Interpolated velocities
-#     uf[3] = dx;
-#     uf[4] = dy;
-#
-#     # To check difference between two interpolation approaches:
-#     # Use r to interpolate exit velocities with weighted averaging:
-#     # r = (uf(1)-u(k-1,1))/(u(k,1)-u(k-1,1));
-#     # uf(3) = uf(3) - (u(k-1,3) + r*(u(k,3) - u(k-1,3)));
-#     # uf(4) = uf(4) - (u(k-1,4) + r*(u(k,4) - u(k-1,4)));
-#
-# end
-#
-# uf[3] = uf[3]/sqrt(uf[3]^2 + uf[4]^2);
-# uf[4] = uf[4]/sqrt(uf[3]^2 + uf[4]^2);
-#
-# return uf;
-#
-# end
 
 
 ###############################################################
 
 
-function SGscatteringrelation(Nedge, Nangle, ds)
+function SGscatteringrelation(thetaTF,metric,Nedge, Nangle, ds)
 # Gather the exit data for the incidence data along the
 # West(left),North(top),East(right),South(bottom) edges.
 
@@ -218,19 +170,36 @@ uS = Array{Array}((Nedge-1),(Nangle-1));
 uE = Array{Array}((Nedge-1),(Nangle-1));
 # For the cells, each row is a point on the boundary edge and each collumn is
 # an angle of incidence.
+if thetaTF == false
+  for i = 1:Nedge-1
+      ds = dl*2*i*(Nedge-i)/Nedge;   # For bad points near corners.
+      for j = 1:Nangle-1
+          u0 = [-1; 1-i*dl; cos(j*dphi - pi/2); sin(j*dphi - pi/2)];   # Only for left edge!
+          uW[i,j] = squaregaussianrelation(metric,u0,ds);
+          u0 = [-1 + i*dl; -1; cos(j*dphi); sin(j*dphi)];   # Only for bottom edge!
+          uS[i,j] = squaregaussianrelation(metric,u0,ds);
+          u0 = [1; -1+i*dl; cos(j*dphi + pi/2); sin(j*dphi + pi/2)];   # Only for right edge!
+          uE[i,j]= squaregaussianrelation(metric,u0,ds);
+          u0 = [1 - i*dl; 1; cos(-j*dphi); sin(-j*dphi)];   # Only for top edge!
+          uN[i,j] = squaregaussianrelation(metric,u0,ds);
+      end
+  end
+end
 
-for i = 1:Nedge-1
-    ds = dl*2*i*(Nedge-i)/Nedge;   # For bad points near corners.
-    for j = 1:Nangle-1
-        u0 = [-1; 1-i*dl; cos(j*dphi - pi/2); sin(j*dphi - pi/2)];   # Only for left edge!
-        uW[i,j] = squaregaussianrelation(u0,ds);
-        u0 = [-1 + i*dl; -1; cos(j*dphi); sin(j*dphi)];   # Only for bottom edge!
-        uS[i,j] = squaregaussianrelation(u0,ds);
-        u0 = [1; -1+i*dl; cos(j*dphi + pi/2); sin(j*dphi + pi/2)];   # Only for right edge!
-        uE[i,j]= squaregaussianrelation(u0,ds);
-        u0 = [1 - i*dl; 1; cos(-j*dphi); sin(-j*dphi)];   # Only for top edge!
-        uN[i,j] = squaregaussianrelation(u0,ds);
-    end
+if thetaTF == true
+  for i = 1:Nedge-1
+      ds = dl*2*i*(Nedge-i)/Nedge;   # For bad points near corners.
+      for j = 1:Nangle-1
+          u0 = [-1;1-i*dl;j*dphi-pi/2];   # 3-vars Left
+          uW[i,j] = squaregaussianrelation(metric,u0,ds);
+          u0 = [-1+i*dl;-1;j*dphi];   # 3-vars Bottom
+          uS[i,j] = squaregaussianrelation(metric,u0,ds);
+          u0 = [1;-1+i*dl;j*dphi+pi/2];   # 3-vars Right
+          uE[i,j]= squaregaussianrelation(metric,u0,ds);
+          u0 = [1-i*dl;1;-j*dphi];   # 3-vars Top
+          uN[i,j] = squaregaussianrelation(metric,u0,ds);
+      end
+  end
 end
 
 return uW, uS, uE, uN;
